dc32552730334d9dfba2feed9d69c9d34a4be46c
MartinViN16 <martgics96@gmail.com>
2026-04-16 22:12:09 -0500

    Actualización

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import java.util.List;
 public class App {
     public static void main(String[] args) throws Exception {
         int cantidadEstudiantes = 20000000;
+        int cantidadConsultas = 40_000;
     
 
         System.out.println("Generando Estudiantes....");
@@ -11,8 +12,6 @@ public class App {
             System.out.println(est.nombre);
         }
 
-        // BUSCAR SI EXISTE EL USR
-        // CON CI 1_999_993
 
         String cedulaBuscar = "1999993";
         for(Estudiante est : estudiantes){
@@ -21,20 +20,21 @@ public class App {
         }
         }
 
-        //int inicio = 0;
-        //int fin = estudiantes.size()-1;
+        int inicio = 0;
+        int fin = estudiantes.size()-1;
 
-        //while (inicio <= fin){
-        // int medio = (inicio + fin)/2;
-        //    Estudiante actual = estudiantes.get(medio);
-        //    int comparacion = actual.cedula.compareTo(cedulaBuscar);
-        //    if(comparacion == 0){
-        //        System.out.println("EXISTE");break;
-        //    }else if (comparacion  < 0){
-        //        inicio = medio + 1;
-        //    }else{
-        //        fin = medio-1;
-        //    }
-        //}
+        while (inicio <= fin){
+         int medio = (inicio + fin)/2;
+            Estudiante actual = estudiantes.get(medio);
+            int comparacion = actual.cedula.compareTo(cedulaBuscar);
+            if(comparacion == 0){
+                System.out.println("EXISTE");break;
+            }else if (comparacion  < 0){
+                inicio = medio + 1;
+            }else{
+                fin = medio-1;
+                
+            }
+        }
     }       
-}
+}       
